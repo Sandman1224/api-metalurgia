17ee58534c51d4a95c2cdfe35c9efe8e550d5df5
Martin Ganami <martinganami@gmail.com>
2021-05-04 23:11:04 -0300

    login endpoint implementation and fixes over configuration file

--- server/config/config.js
+++ server/config/config.js
@@ -12,5 +12,15 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // ====================
 // Base de datos
 // ====================
-process.env.URLDB = process.env.URLDB || 'mongodb://localhost:27017/cafe';
+const dbName = 'metallurgy';
+process.env.URLDB = process.env.URLDB || `mongodb://localhost:27017/${dbName}`;
 
+// ====================
+// Vencimiento del Token
+// ====================
+process.env.CADUCIDAD_TOKEN = '48h';
+
+// ====================
+// SED de autenticación
+// ====================
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';

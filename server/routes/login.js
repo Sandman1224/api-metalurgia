2ca2de9002c15da260a2eb9fda268ccf726fe27b
Martin Ganami <martinganami@gmail.com>
2021-07-19 23:55:16 -0300

    implementing machine data on login controls and date result

--- server/routes/login.js
+++ server/routes/login.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
+const Machine = require('../models/machines')
 
 const app = express();
 
@@ -35,14 +36,48 @@ app.post('/login', (req, res) => {
             })
         }
 
-        let token = jwt.sign({
-            user: userDb,
-        }, process.env.SEED, {expiresIn: process.env.CADUCIDAD_TOKEN});
+        // Control de Ip del dispositivo
+        Machine.findOne({ devices: body.deviceIp, status: 1 }, (errorMachine, machineDb) => {
+            if (errorMachine) {
+                return res.status(500).json({
+                    ok: false,
+                    error
+                })
+            }
 
-        res.json({
-            ok: true,
-            userData: userDb,
-            token
+            if (!machineDb) {
+                return res.status(400).json({
+                    ok: false,
+                    error: {
+                        message: 'Device Ip does not correct.'
+                    }
+                })
+            }
+
+            let token = jwt.sign({
+                user: userDb,
+            }, process.env.SEED, {
+                expiresIn: process.env.CADUCIDAD_TOKEN
+            })
+
+            const dataResult = {
+                userId: userDb._id,
+                userRole: userDb.role,
+                userFullname: userDb.fullname,
+                userLastname: userDb.userLastname,
+                userEmployeeId: userDb.employeeId,
+                machine: {
+                    id: machineDb._id,
+                    name: machineDb.name,
+                    identifier: machineDb.identifier
+                }
+            }
+
+            res.json({
+                ok: true,
+                userData: dataResult,
+                token
+            })
         })
     });
 });

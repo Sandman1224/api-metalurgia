7dcf4c9ec02ee0e315fb4506d6b5c35e148efa9f
Martin Ganami <martinganami@gmail.com>
2021-08-12 23:41:33 -0300

    Piece code formatted

--- server/facade/pieces-facade.js
+++ server/facade/pieces-facade.js
@@ -39,7 +39,18 @@ function getPieceCode(machineCode, currentMonth, pieceNumber) {
             break
     }
 
-    return `${ machineCode }${ monthCode }-${ pieceNumber + 1 }`
+    const formattedNumber = zeroFill(pieceNumber + 1, 4)
+    return `${ machineCode }${ monthCode }-${ formattedNumber }`
+}
+
+function zeroFill(number, width)
+{
+  width -= number.toString().length;
+  if ( width > 0 ){
+    return new Array( width + (/\./.test( number ) ? 2 : 1) ).join( '0' ) + number;
+  }
+
+  return number + "";
 }
 
 module.exports = {

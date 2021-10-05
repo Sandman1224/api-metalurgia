6ac1bb627730eb582fcd5e0929b4f3bc94c8d8a9
Martin Ganami <martinganami@gmail.com>
2021-10-04 23:45:29 -0300

    updating get users endpoint

--- server/routes/v0/user.js
+++ server/routes/v0/user.js
@@ -3,6 +3,43 @@ const User = require('../../models/user')
 
 const app = express()
 
+app.get('/users', (req, res) => {
+    let page = parseInt(req.query.page) || 0
+    let limit = parseInt(req.query.limit) || 1
+
+    const query = { status: { $gt: -1 } }
+
+    User.find(query)
+        .sort({ created: -1 })
+        .skip(page * limit)
+        .limit(limit)
+        .exec((error, usersDb) => {
+            if (error) {
+                return res.status(500).json({
+                    ok: false,
+                    error
+                });
+            }
+
+            User.countDocuments(query).exec((counterError, countDb) => {
+                if (error) {
+                    return res.status(500).json({
+                        ok: false,
+                        error
+                    });
+                }
+
+                return res.json({
+                    ok: true,
+                    total: countDb,
+                    page: page,
+                    pageSize: usersDb.length,
+                    data: usersDb
+                })
+            })
+        })
+})
+
 /**
  * Buscar un usuario por "Id"
  */

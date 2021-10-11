859f3afc63b9155b6f090620f0d01fe40e1451f1
Martin Ganami <martinganami@gmail.com>
2021-10-11 17:03:46 -0300

    Machine functionalities
    Implementing endpoints for all the machines functionalities on admin panel

--- server/models/machines.js
+++ server/models/machines.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 let Schema = mongoose.Schema
 
@@ -14,11 +15,24 @@ let machineSchema = new Schema({
     },
     devices: {
         type: String,
-        required: [true, 'Machine ip device is mandatory']
+        unique: true,
+        validate: {
+            validator: function(ipData) {
+                return /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipData)
+            },
+            message: '{VALUE} no es un valor correcto'
+        },
+        required: [true, 'Machine ip device is mandatory'],
     },
     status: {
-        type: Number
-    }
+        type: Number,
+        required: [true, 'Machine status device is mandatory']
+    },
+    description: String
 })
+
+machineSchema.index({ identifier: 1, status: 1 }, { unique: true })
+
+machineSchema.plugin(uniqueValidator, { message: 'El identificador de máquina ya se encuentra registrado' })
 
 module.exports = mongoose.model('Machine', machineSchema)
